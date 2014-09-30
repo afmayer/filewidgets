86f8586d1eb3c7e11614ae7a7f3e7de6d0cd5c58
Alexander F. Mayer <tinybluebutton@gmail.com>
2014-09-30 11:25:50 +0200

    Tcl: Fix packages "dde" and "registry" when used from a slave interpreter.
    * these packages are statically built-in
    * main interpreter has these ready without a "package require" because this
        is done on application initialization
    * a "load" within a slave interpreter (invoked due to "package require") of
        one of these DLLs failed because it does not exist
    * statically linked packages can be loaded by specifying an empty filename
        --> this is the bugfix

--- tcl/lib/tcl8.6/reg/pkgIndex.tcl
+++ tcl/lib/tcl8.6/reg/pkgIndex.tcl
@@ -2,8 +2,8 @@ if {([info commands ::tcl::pkgconfig] eq "")
 	|| ([info sharedlibextension] ne ".dll")} return
 if {[::tcl::pkgconfig get debug]} {
     package ifneeded registry 1.3.0 \
-            [list load [file join $dir tclreg13g.dll] registry]
+            [list load "" registry]
 } else {
     package ifneeded registry 1.3.0 \
-            [list load [file join $dir tclreg13.dll] registry]
+            [list load "" registry]
 }
